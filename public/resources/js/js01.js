9f4039be854de256e407c7f4f4a6376c7f85088e
Paul Ramnora <pramnora@yahoo.com>
2026-08-29 11:09:15 +0100

    Add documentation comments to js01.js
    Added header comments for documentation.

--- public/resources/js/js01.js
+++ public/resources/js/js01.js
@@ -1,1 +1,8 @@
-alert(0) //...Just testing
+/* 
+ Javascript code for web site: Firebase 01 
+ -----------------------------------------
+ CREATED: Sat 29 Aug 2026 11:08 AM GMT
+ UPDATED: Sat 29 Aug 2026 11:08 AM GMT
+*/
+
+console.log("Test site: Firebase 01");   
